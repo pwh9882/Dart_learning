7169f541c0d9bed3ad157ea64c78777315379020
박우혁 <woohyeok@bag-uhyeog-ui-MacBookPro.local>
2022-12-16 15:52:02 +0900

    변수에 관해 배움

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 1. dart의 main function
 
+2. 변수선언: var & type(ex: String...) 둘 다 가능하나
+    함수나 메소드에서는 var, class에는 타입명시를 주로 한다.
--- main.dart
+++ main.dart
@@ -1,3 +1,10 @@
 void main(List<String> args) {
+  var testing_variable = "test";
+  // testing_variable = "1"; //불가능-type이 달라질 수 없다
+  testing_variable = "ham";
+  // class: type을 지정해서
+  // function 등의 지역변수: var를 사용해서
+
   print("Hello World!");
+  print("print testing_variable: " + testing_variable);
 }
