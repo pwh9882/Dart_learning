57c6d2495d1e250596acef67d4a96b736207c589
박우혁 <woohyeok@bag-uhyeog-ui-MacBookPro.local>
2022-12-16 16:02:52 +0900

    dynamic variable 공부

--- main.dart
+++ main.dart
@@ -7,4 +7,22 @@ void main(List<String> args) {
 
   print("Hello World!");
   print("print testing_variable: " + testing_variable);
+
+  print("about dynamic variables");
+  dynamic_variables();
+}
+
+void dynamic_variables() {
+  var test1;
+  dynamic test2;
+  test1 = "asdf";
+  test2 = true;
+  test2 = 123;
+
+  if (test1 is String) {
+    print(test1.length);
+  }
+  if (test2 is int) {
+    print(test2.isEven);
+  }
 }
